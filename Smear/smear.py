7f13c1e9b5f1c515e8435df18671f851bb50543c
lwunderl <luke.wunderlin@gmail.com>
2023-09-05 16:50:38 -0500

    modified deck information

--- Smear/smear.py
+++ Smear/smear.py
@@ -2,20 +2,31 @@
 from random import shuffle
 
 #Create number of players
-player_count = int(input("How many players? "))
+def choose_number_of_players():
+    player_count = int(input("How many players? "))
+    return player_count
 
 #Create card deck
-suits = ["Spades","Hearts","Diamonds","Clubs"]
-facecards = ["Ace","King","Queen","Jack"]
-deck = [f"{x} of {y}" for x in range(2,11) for y in suits]
-deck.extend([f"{x} of {y}" for x in facecards for y in suits])
-deck.extend(["Joker","Joker"])
-
-#Shuffle deck
-shuffle(deck)
+def create_deck():
+    suits = ["Spades","Hearts","Diamonds","Clubs"]
+    facecards = ["Ace","King","Queen","Jack"]
+    deck = [{"card_name": f"{x} of {y}", "card_number": x, "card_suit": y, "card_value": 0} for x in range(2,11) for y in suits]
+    deck.extend([{"card_name": f"{x} of {y}", "card_number": x, "card_suit": y, "card_value": 0} for x in facecards for y in suits])
+    deck.extend([{"card_name": "Joker", "card_number": "Joker", "card_suit": "Joker", "card_value": 0}, {"card_name": "Joker", "card_number": "Joker", "card_suit": "Joker", "card_value": 0}])
+    for card in deck:
+        if card["card_number"] == 10:
+            card["card_value"] = 10
+        if card["card_number"] == "Ace":
+            card["card_value"] = 4
+        if card["card_number"] == "King":
+            card["card_value"] = 3
+        if card["card_number"] == "Queen":
+            card["card_value"] = 2
+        if card["card_number"] == "Jack":
+            card["card_value"] = 1
+    return deck
 
 #Create dealing
-
 def deal(deck, player_count):
     hands = []
     cards_per_hand = round(len(deck)/player_count)
@@ -30,12 +41,41 @@ def deal(deck, player_count):
     return hands
 
 #Create bidding
+def bid(player_count):
+    winning_bidder = ""
+    high_bid = 0
+    for _ in range(player_count):
+        bid = int(input(f"Player {_ + 1}'s bid? "))
+        if bid > high_bid:
+            high_bid = bid
+            winning_bidder = f"Player {_ + 1}"
+    return {"winning_bidder": winning_bidder, "winning_bid": high_bid}
+
+def declare_trump(winning_bid):
+    trump = input(f"What suit is trump? ")
+    winning_bid["trump_suit"] = trump
+    return winning_bid
+
+def discard(hands, winning_bid):
+    trump = winning_bid["trump_suit"]
+    for hand in hands:
+        cards = list(hand.values())[0]
+        for card in cards:
+            print(card)
+            
 
 #Create card-play
 
+
 #Create scoring
 
+
+#Run game
 def main():
+    player_count = choose_number_of_players()
+    deck = create_deck()
+    #Shuffle deck
+    shuffle(deck)
     hands = deal(deck, player_count)
     print(hands[0])
 
