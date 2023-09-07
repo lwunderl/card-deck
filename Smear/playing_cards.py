cab97d59550c6b51728b88a32bfaf2fa92b29470
lwunderl <luke.wunderlin@gmail.com>
2023-09-07 12:52:27 -0500

    added bid class with methods and dealing and discard methods to card deck class

--- Smear/playing_cards.py
+++ Smear/playing_cards.py
@@ -10,10 +10,12 @@ class PlayingCard:
 
 #create playing card deck object class
 class CardDeck:
-    def __init__(self):
+    def __init__(self, name):
+        self.name = name
         self.cards = []
 
     #create a deck of cards
+    #game is a string
     def build_deck(self, game):
         self.game = game
 
@@ -49,52 +51,85 @@ class CardDeck:
             self.cards.extend([PlayingCard(f"{x} of {y}", x, y, 0) for x in range(2,11) for y in suits])
             self.cards.extend([PlayingCard(f"{x} of {y}", x, y, 0) for x in facecards for y in suits])
     
-    #create draw card method to remove playing card objects from card deck object
+    #create draw card method to remove playing card objects from card deck object and return drawn cards
+    #n is an integer
     def draw_cards(self, n):
         draw = self.cards[:n]
         self.cards = self.cards[n:len(self.cards)]
         return draw
     
-    #create discard card method to add playing card objects do a card deck object
-    def discard_cards(self, cards):
-        discard = cards
-        self.cards = self.cards.extend(cards)
-        return discard
+    #create discard card method to remove specific playing card objects from card deck object and place into separate card deck object
+    #cards must be a list of playing card objects example: [player_hand.cards[2]] or player_hand.cards, deck must be a card deck object in which to discard to.
+    def discard_cards(self, cards, deck):
+        for card in cards:
+            self.cards.remove(card)
+        deck.cards.extend(cards)
+    
+    #create a method to deal player card objects from card deck object to player hand objects
+    #n is an integer and the number of cards you would like to deal in each pass, hands is a list of player hand objects, p is an integer for the number of dealing passes to make
+    def deal_cards(self, n, hands, p=1):
+        for _ in range(p):
+            for hand in hands:
+                hand.draw_hand(n, self)
 
-#create player hand object class
+#create player hand object subclass of card deck class
 class PlayerHand(CardDeck):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, name):
+        super().__init__(name)
         self.cards = []
     
-    #create method to draw playing card objects from card deck object
+    #create method to draw playing card objects from card deck object and place into player hand object
+    #n must be integer, deck must be card deck object
     def draw_hand(self, n, deck):
         draw = deck.draw_cards(n)
         self.cards.extend(draw)
-    
-    def discard_hand(self, cards):
-        ...
 
 #create bidding mechanic class 
 class Bid:
-    def __init__(self, game):
-        self.game = game
+    def __init__(self):
+        self.highest_bid = 0
+        self.trump = ""
+        self.winning_bidder = ""
 
-    def declare_trump(self):
-        ...
+    def bid(self, hands):
+        for hand in hands:
+            bid = int(input(f"{hand.name}'s bid? "))
+            if bid > self.highest_bid:
+                self.highest_bid = bid
+                self.winning_bidder = hand.name
+
+    def declare_trump(self, suit):
+        self.trump = suit
 
 #main script
 def main():
-    smear_deck = CardDeck()
+    smear_deck = CardDeck("smear_deck")
     smear_deck.build_deck("smear")
-    print([smear_deck.cards[x].name for x in range(len(smear_deck.cards))],"\n")
-    player_hand = PlayerHand()
-    player_hand.draw_hand(10, smear_deck)
-    #draw = smear_deck.draw_cards(5)
-    #player_hand.build_hand(draw)
-    print([player_hand.cards[x].name for x in range(len(player_hand.cards))],"\n")
-    print([smear_deck.cards[x].name for x in range(len(smear_deck.cards))],"\n")
+    # print([smear_deck.cards[x].name for x in range(len(smear_deck.cards))],"\n")
 
+    discard_pile = CardDeck("discard_pile")
+    # print([discard_pile.cards[x].name for x in range(len(discard_pile.cards))],"\n")
+
+    player1_hand = PlayerHand("player_1")
+    player2_hand = PlayerHand("player_2")
+    player3_hand = PlayerHand("player_3")
+
+    all_hands = [player1_hand, player2_hand, player3_hand]
+
+    bid = Bid()
+    bid.bid(all_hands)
+    bid.declare_trump(input(f"{bid.winning_bidder}, please select a trump. "))
+    print(bid.winning_bidder, bid.trump)
+
+    # smear_deck.deal_cards(2, all_hands, 2)
+    # player_hand.draw_hand(10, smear_deck)
+    # print([player1_hand.cards[x].name for x in range(len(player1_hand.cards))],"\n")
+    # print([smear_deck.cards[x].name for x in range(len(smear_deck.cards))],"\n")
+
+    # player_hand.discard_cards([player_hand.cards[1]], discard_pile)
+    # print([player_hand.cards[x].name for x in range(len(player_hand.cards))],"\n")
+    # print([smear_deck.cards[x].name for x in range(len(smear_deck.cards))],"\n")
+    # print([discard_pile.cards[x].name for x in range(len(discard_pile.cards))],"\n")
 
 if __name__ == "__main__":
     main()
--- Smear/smear.py
+++ Smear/smear.py
@@ -1,68 +1,30 @@
 #import dependencies
 from random import shuffle
-from playing_cards import CardDeck
+import playing_cards as pc
 
 #Create number of players
 def choose_number_of_players():
     player_count = int(input("How many players? "))
     return player_count
 
-#Create dealing
-def deal(deck, player_count):
-    hands = []
-    cards_per_hand = round(len(deck)/player_count)
-    if cards_per_hand > 10:
-        cards_per_hand = 10
-    cards_dealt = 0
-    for player in range(player_count):
-        player_number = player + 1
-        hand = [deck[x + player + cards_dealt] for x in range(cards_per_hand)]
-        cards_dealt += cards_per_hand - 1
-        hands.append({f"Player {player_number} Hand":hand})
-    return hands
-
 #Create bidding
-def bid(player_count):
-    winning_bidder = ""
-    high_bid = 0
-    for _ in range(player_count):
-        bid = int(input(f"Player {_ + 1}'s bid? "))
-        if bid > high_bid:
-            high_bid = bid
-            winning_bidder = f"Player {_ + 1}"
-    return {"winning_bidder": winning_bidder, "winning_bid": high_bid}
-
-def declare_trump(winning_bid):
-    trump = input(f"What suit is trump? ")
-    winning_bid["trump_suit"] = trump
-    return winning_bid
-
-def discard(hands, winning_bid):
-    discard_pile = []
-    for hand in hands:
-        cards = list(hand.values())[0]
-        for card in cards:
-            if card["card_suit"] == winning_bid["trump_suit"]:
-                print(card)
-            else:
-                discard_pile.append(card)
-        print(discard_pile)
             
 #Create card-play
 
-
 #Create scoring
-
 
 #Run game
 def main():
     player_count = choose_number_of_players()
     #Create card deck
-    deck = CardDeck("smear")
-    deck.build_deck()
+    deck = pc.CardDeck("smear_deck")
+    deck.build_deck("smear")
     #Shuffle deck
     shuffle(deck.cards)
+    hand = pc.PlayerHand("player1_hand")
+    hand.draw_hand(2, deck)
     print(deck.cards[0].name)
+    print(hand.cards[0].name)
 
 if __name__ == "__main__":
     main()
