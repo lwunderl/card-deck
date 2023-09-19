6b333906a77d561a7ad6b5abd5066ccb3e30eed0
lwunderl <luke.wunderlin@gmail.com>
2023-09-19 16:16:35 -0500

    added basic game set-up, refined team and trick objects, added an is_trump method to card objects

--- Smear/playing_cards.py
+++ Smear/playing_cards.py
@@ -10,12 +10,32 @@ class PlayingCard:
         self.color = str(color)
         self.order = int(order)
 
+    def __str__(self):
+        return f"Playing card named {self.name}"
+    
+    def is_trump(self, trump):
+        black = ["Spades", "Clubs"]
+        red = ["Hearts", "Diamonds"]
+        if self.suit == trump:
+            return True
+        elif self.id == "Jack" and self.suit in black and trump in black:
+            return True
+        elif self.id == "Jack" and self.suit in red and trump in red:
+            return True
+        elif self.id == "Joker":
+            return True
+        else:
+            return False
+
 #create playing card deck object class
 class CardDeck:
     def __init__(self, name):
         self.name = name
         self.suits = []
         self.cards = []
+    
+    def __str__(self):
+        return f"Card deck named {self.name}"
 
     #create a card deck object full of playing card objects
     #game is a string
@@ -91,8 +111,8 @@ class CardDeck:
     
     #create a method to deal player card objects from card deck object to player hand objects
     #n is an integer and the number of cards you would like to deal in each pass, hands is a list of player hand objects, p is an integer for the number of dealing passes to make
-    def deal_cards(self, n, hands, p=1):
-        for _ in range(p):
+    def deal_cards(self, n, hands, passes=1):
+        for _ in range(passes):
             for hand in hands:
                 hand.draw_hand(n, self)
     
@@ -107,7 +127,7 @@ class Hand(CardDeck):
         self.cards = []
     
     #create method to draw playing card objects from card deck object and place into player hand object
-    #n must be integer, deck must be card deck object
+    #n must be integer, deck must be card deck object or subclass
     def draw_hand(self, n, deck):
         draw = deck.draw_cards(n)
         self.cards.extend(draw)
@@ -122,45 +142,52 @@ class Trick(CardDeck):
         self.game_points = 0
         self.high = 0
         self.low = 0
+    
+    def __str__(self):
+        return f"Trick Name: {self.name}, Trick Cards: {self.cards}"
 
-    #search through the trick to add up the card values for the "game" score point
+    #search through the trick to add up the card values for the "game" score point in smear
     def game_points(self):
         for card in self.cards:
             if card.id in ["Ace","King","Queen","Jack","10"]:
                 self.game_points += card.value
     
-    #search through the trick to find the highest trump card for the "high" score point
+    #search through the trick to find the highest trump card for the "high" score point in smear based on playing card object self.order
     def high(self):
         order = 0
         for card in self.cards:
             if card.suit == self.trump and card.order > order:
                 self.high = card.order
     
-    #search through the trick to find the lowest trump card for the "low" score point
+    #search through the trick to find the lowest trump card for the "low" score point in smear based on playing card object self.order
     def low(self):
         order = 0
         for card in self.cards:
             if card.suit == self.trump and card.order < order:
                 self.low = card.order
     
-    #search through the trick to find the "Jack", "Jick", and "Jokers" score points
+    #search through the trick to find the "Jack", "Jick", and "Jokers" for smear
     def jjj(self):
-        black = ["Spades", "Clubs"]
-        red = ["Hearts", "Diamonds"]
         for card in self.cards:
-            if card.id == "Jack" and card.suit in black and self.trump in black:
-                self.score_cards.extend(card.name)
-            elif card.id == "Jack" and card.suit in red and self.trump in red:
-                self.score_cards.extend(card.name)
+            if card.id == "Jack" and card.is_trump(self.trump):
+                self.score_cards.append(card.name)
             elif card.id == "Joker":
-                self.score_cards.extend(card.name)
+                self.score_cards.append(card.name)
 
 #create a player class object
 class Player:
     def __init__(self, name):
         self.name = name
+        self.hand = ""
+        self.npc = False
+    
+    def __str__(self):
+        return f"{self.name}"
+    
+    def get_hand(self, hand):
+        self.hand = hand
 
-#create a team class object which will contain player objects and trick objects
+#create a team class object which will contain player objects and trick objects, players will not have a score, they must be placed on a team even if it is a team of 1 player.
 class Team:
     def __init__(self, name):
         self.name = name
@@ -168,20 +195,28 @@ class Team:
         self.tricks = []
         self.score = 0
     
+    def __str__(self):
+        return f"Team Name: {self.name}, Players: {[x.name for x in self.players]}, Tricks: {[x.name for x in self.tricks]}, Score: {self.score}"
+    
     #attach trick to team
     def get_trick(self, trick):
-        self.trick.extend(trick)
+        self.trick.append(trick)
 
     #attach player to team
-    def get_player(self, player):
-        self.player.extend(player)
+    def add_player(self, player):
+        self.players.append(player)
 
-class Score:
-    def __init__(self):
-        self.score = 0
+    #add score
+    def add_score(self, n):
+        self.score += n
     
-    def score_tricks():
-        ...
+    #subtract score
+    def subract_score(self, n):
+        self.score -= n
+
+    def score_tricks(self):
+        for trick in self.tricks:
+            print(len(trick.score_cards), trick.game_points, trick.high, trick.low)
 
 #create bidding object class 
 class Bid:
@@ -190,42 +225,39 @@ class Bid:
         self.trump = ""
         self.winning_bidder = ""
 
-    #make a bid using player object
-    def make_bid(self, player):
+    def __str__(self):
+        return f"Winning Bidder: {self.winning_bidder}, Bid: {self.highest_bid}, Trump: {self.trump}"
+
+    #make a bid using player object, and maximum bid allowed
+    def make_bid(self, player, max=7):
         while True:
             try:
                 bid = int(input(f"{player.name}'s bid? "))
-                if bid > self.highest_bid:
+                if bid > self.highest_bid and bid <= max:
                     self.highest_bid = bid
-                    self.winning_bidder = player.name
-                break
+                    self.winning_bidder = player
+                    break
+                elif bid <= self.highest_bid:
+                    print("Bid not high enough, next!")
+                    break
+                else:
+                    print(f"{max} is the maximum allowable bid!")
             except ValueError:
-                print("Bid needs to be a number")
+                print("Bid needs to be an integer!")
 
-    #declare trump with suit as string
-    def declare_trump(self, suit):
-        self.trump = suit
-        print(f"Trump is {suit}!")
-    
-    #display the bid information
-    def display_bid(self):
-        print(f"Winning Bidder: {self.winning_bidder}, Bid: {self.highest_bid}, Trump: {self.trump}")
+    #declare trump using deck suits
+    def declare_trump(self, deck):
+        while True:
+            trump = input(f"{self.winning_bidder}, declare trump suit {deck.suits}: ")
+            if trump in deck.suits:
+                self.trump = trump
+                break
+            else:
+                print("Trump must be a suit in the deck!")
 
 #main script
 def main():
-    smear_deck = CardDeck("smear")
-    smear_deck.build_deck("smear")
-    player_hand = Hand("player_hand")
-    player_hand.draw_hand(50,smear_deck)
-    player_hand.display_cards()
-
-    bid = Bid()
-    bid.declare_trump("Hearts")
-
-    trick = Trick("trick_1", bid)
-    player_hand.discard_cards(player_hand.cards[:40], trick)
-    trick.display_cards()
-
+    ...
 
 if __name__ == "__main__":
     main()
--- Smear/smear.py
+++ Smear/smear.py
@@ -4,20 +4,73 @@ import playing_cards as pc
 
 #Run game
 def main():
-    ...
-    #enter game
+    #Create players and teams, there are always 2 teams in smear
+    number_of_players = int(input("How many players? "))
+    players = []
+    for player in range(1,number_of_players + 1):
+        players.append(pc.Player(f"Player_{player}"))
+    
+    team_1 = pc.Team("Team_1")
+    team_2 = pc.Team("Team_2")
+    i = 0
+    for player in players:
+        if i % 2 == 0:
+            team_1.add_player(player)
+        else:
+            team_2.add_player(player)
+        i += 1
+
     #Create card deck
-
-    #Create players
-
-    #Create teams
+    deck = pc.CardDeck("Smear Deck")
+    deck.build_deck("smear")
+    
+    discard_pile = pc.CardDeck("Discard Pile")
 
     #Create hands
+    hands = []
+    for hand in range(1,number_of_players + 1):
+        hands.append(pc.Hand(f"Hand_{hand}"))
+
     #Deal hands
+    shuffle(deck.cards)
+    deck.deal_cards(1, hands, 10)
+
+    #Players pick up hands
+    i = 0
+    for player in players:
+        player.get_hand(hands[i])
+        i += 1
 
     #Make bids
+    bid = pc.Bid()
+    for player in players:
+        bid.make_bid(player)
+    
+    #declare trump of the deck
+    bid.declare_trump(deck)
+    
+    #discard non-trump cards
+    for player in players:
+        non_trump = []
+        for card in player.hand.cards:
+            if card.is_trump(bid.trump):
+                continue
+            else:
+                non_trump.append(card)
+        player.hand.discard_cards(non_trump, discard_pile)
+    
+    #shuffle discard pile
+    shuffle(discard_pile.cards)
 
-    #discard, shuffle, redraw step
+    #draw to 7 cards in hand
+    for player in players:
+        while True:
+            if len(player.hand.cards) < 7 and len(deck.cards) > 0:
+                player.hand.draw_hand(1,deck)
+            elif len(player.hand.cards) < 7 and len(discard_pile.cards) > 0:
+                player.hand.draw_hand(1,discard_pile)  
+            else:
+                break
                 
     #Create card-play
     #Store tricks
